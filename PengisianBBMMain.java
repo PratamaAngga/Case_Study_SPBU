416c24667be2619f33e4ec3189dbecefad1b925c
ganangputra31 <myinwork14@gmail.com>
2025-05-27 13:39:12 +0700

    Main

---
+++ PengisianBBMMain.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class PengisianBBMMain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        AntrianKendaraan Antrian = new AntrianKendaraan();
+        
+
+
+        int pilihan;
+        do {
+            System.out.println("\n --- Menu SPBU ---");
+            System.out.println("1. Tambah Antrian Kendaraan ");
+            System.out.println("2. Tampilkan Antrian ");
+            System.out.println("3. Cek Jumlah Antrian Kendaraan ");
+            System.out.println("4. Layani Kendaraan ");
+            System.out.println("5. Tampilkan Riwayat Transaksi ");
+            System.out.println("0. Keluar ");
+            System.out.print("Pilih : ");
+            pilihan = sc.nextInt();
+            sc.nextLine();
+
+            switch (pilihan) {
+                case 1:
+                    System.out.print("Masukkan Plat Nomor  : ");
+                    String platNomor = sc.nextLine();
+                    System.out.print("Masukkan Jenis Kendaraan : ");
+                    String tipe = sc.nextLine();
+                    System.out.print("Masukkan Merk  : ");
+                    String merk = sc.nextLine();
+            }
+        }
+
+
+    }
+    
+}
